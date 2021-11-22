43f4576d05a2367aeb0de33882af366a8604b0af
Sameera Gurusinghe <49789150+Guruz1@users.noreply.github.com>
2021-11-22 10:41:21 +0530

    Conditions

---
+++ conditions.py
@@ -0,0 +1,11 @@
+temperature = 26
+
+if temperature > 30:  # >30
+    print("It's hot day. Drink plenty of water.")
+elif temperature > 20:  # 20~30
+    print("It's nice day.")
+elif temperature > 10:  # 10~20
+    print("It's bit cold day.")
+else:  # >10
+    print("It's cold day.")
+print("Done")
