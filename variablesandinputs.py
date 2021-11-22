a576c0f19f209b58b0528f6b38d38a421320e4b7
Sameera Gurusinghe <49789150+Guruz1@users.noreply.github.com>
2021-11-22 10:03:57 +0530

    Variables and input

---
+++ variablesandinputs.py
@@ -0,0 +1,19 @@
+# Print hello world...
+print('Hello World')
+
+# name = input('Enter your name ')
+# print('Hello,' + name)
+
+# age = 20
+# age = 30
+# age = 40
+# print(age)
+
+# birth_year = input('Enter your birth year: ')
+# age = 2020 - int(birth_year)
+# print(age)
+
+first = input('first number: ')
+second = input('second number: ')
+sum = float(first) + float(second)
+print('Sum is: ' + str(sum))
